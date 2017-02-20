89a140ba712c32e964abd49c43167ecf1615714c
Vitaliy <vitaliy@192.168.100.33>
2017-02-20 21:40:59 +0300

    do improvemnts to make index.js reusable

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
 
-import brainGame from '..'
+import { printMessage, askQuestion} from '../index'
+
+printMessage('Welcome to the Brain Games!\n');
+const userName = askQuestion('May I have your name? ');
+printMessage(`Hello ${userName}!`);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!\n');
+export const printMessage = (message) => {
+  console.log(message);
+};
 
-const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello ${userName}!`);
+export const askQuestion = (question) => {
+  return readlineSync.question(question);
+};
